a5448cd32f2318132fce2caf1989999054a680fb
SuneetPanigrahi <suneet.panigrahi@ipsator.com>
2023-09-12 12:36:54 +0530

    added delete functionalities

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,58 @@
-import { useQuery,useMutation } from '@tanstack/react-query'
-import './App.css'
-import Posts from './components/Posts'
+import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
+import "./App.css";
+import Posts from "./components/Posts";
 
 function App() {
-let {data,isLoading,isError}=useQuery({
-  queryKey:["posts"],
-  queryFn:()=>wait(1000).then(async()=>await(await fetch("http://localhost:3000/posts")).json())
-}) 
+  let queryClient = useQueryClient();
+  let { data, isLoading, isError } = useQuery({
+    queryKey: ["posts"],
+    queryFn: () =>
+      wait(1000).then(
+        async () => await (await fetch("http://localhost:3000/posts")).json()
+      ),
+  });
 
-console.log("DATA",data)
-if(isLoading){
-  return <h1>Loading...</h1>
-}
+  //Delete Data
 
-if(isError){
-  return <h1>Something is wrong</h1>
-}
+  let deleteMutation = useMutation({
+    mutationFn: (id) => {
+      return wait(1000).then(() =>
+        fetch(`http://localhost:3000/posts/${id}`, {
+          method: "DELETE",
+        })
+      );
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries(["posts"]);
+    },
+  });
+  console.log("DATA", data);
+  if (isLoading) {
+    return <h1>Loading...</h1>;
+  }
+
+  if (isError) {
+    return <h1>Something is wrong</h1>;
+  }
   return (
     <div>
       <h1>Add Posts</h1>
-      <Posts/>
-       <h1>Posts</h1>
-       {
-        data && data?.map((post)=><li key={post.id}>{post.title}</li>)
-       }
+      <Posts />
+      <h1>Posts</h1>
+      {data &&
+        data?.map((post) => (
+          <li key={post.id}>
+            {post.title}
+            <button onClick={() => deleteMutation.mutate(post.id)}>
+              Delete
+            </button>
+          </li>
+        ))}
     </div>
-  )
+  );
 }
 
-export function wait(delay){
-  return new Promise(resolve=>setTimeout(resolve,delay))
+export function wait(delay) {
+  return new Promise((resolve) => setTimeout(resolve, delay));
 }
-export default App
+export default App;
